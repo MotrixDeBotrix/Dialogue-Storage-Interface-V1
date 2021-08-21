a46dec471e2dab477ff2a09ae70aeeaf1e49332b
MotrixDeBotrix <76997112+MotrixDeBotrix@users.noreply.github.com>
2021-08-21 18:17:27 +0200

    Create MainDialogueStorage.cs

---
+++ MainDialogueStorage.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MainDialogueStorage : MonoBehaviour
+{
+
+    [HideInInspector] public string output = "Knokker Brock";
+}
